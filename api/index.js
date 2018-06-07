5bcba273f6d094d689abb7fedd18631e7a664284
Stefan Benz <stbenz88@gmail.com>
2018-06-07 15:03:07 +0200

    add heroku to project

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/message', (req, res) => {
-	res.send({message: 'Hello World'});
+	res.send({message: 'Hello enterJS'});
 });
 
 app.listen(3001, () => {
